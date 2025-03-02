4e5eb6e674d4bb5765a2f87bde493d7635d2a537
Adamy <admye@ucdavis.edu>
2025-03-01 16:37:21 -0800

    basic jwt

--- server.js
+++ server.js
@@ -17,12 +17,31 @@ const posts = [
     }
 ];
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
 });
 
-app.get('/login', (req, res) => {
-    // Authenticate user
+app.post('/login', (req, res) => {
+    // Authenticate user(waiting for implementation)
+
+
+    // Create a token
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken }); // Send the token to the client
 });
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    // console.log(token);
+    if (token == null) return res.sendStatus(401); // If there is no token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); // If the token is invalid
+        req.user = user;
+        next(); // If the token is valid, continue
+    });
+}
 
 app.listen(PORT);
